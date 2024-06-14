104d907c2b30de390cc7320108159c655a7b47cd
GAURAV DHONGDE <gauravgd16@gmail.com>
2024-06-14 13:26:47 +0200

    Update bot_llama.py

--- bot_llama.py
+++ bot_llama.py
@@ -35,6 +35,11 @@ st.markdown(
     display: none;
     
     }
+
+    a[href="https://streamlit.io/cloud"][target="_blank"][rel="noopener noreferrer"].viewerBadge_container__r5tak.styles_viewerBadge__CvC9N {
+        display: none;
+        
+    }
 </style>
 """,
     unsafe_allow_html=True,
