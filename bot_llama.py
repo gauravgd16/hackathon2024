a22ae5e0328ff514814ec25824d8cc11cdc316af
Gaurav Dhongde <gaurav.gautam.dhongde@sap.com>
2024-07-03 10:13:05 +0200

    Modified code

--- bot_llama.py
+++ bot_llama.py
@@ -1,4 +1,6 @@
 import os
+import string
+import random
 import streamlit as st
 from groq import Groq
 from streamlit_chat import message
@@ -40,13 +42,17 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
+
+
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "You are a compassionate and knowledgeable Mental Health and Wellbeing expert ChatBot of SAP company, dedicated to supporting employees at SAP. Your role involves providing accurate information and empathetic support concerning mental health and general wellbeing. Offer helpful advice, resources, and coping strategies with a focus on care and understanding. Share information about SAP's Mental Health & Wellbeing Program when relevant or upon request, ensuring the employee receives the best possible support tailored to their needs."}]
 
 message("SAP's Mental Health Chatbot is here to help you. How can I assist you today?", is_user=False, avatar_style="bottts", seed=123)
 
 for msg in st.session_state.messages[1:]:
-    message(msg["content"], is_user=msg["role"] == "user", avatar_style="avataaars" if msg["role"] == "user" else "bottts", seed=123)
+    key = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k=10))
+    message(msg["content"], is_user=msg["role"] == "user", avatar_style="avataaars" if msg["role"] == "user" else "bottts", seed=123,key=key)
 
 if prompt := st.chat_input():
     if not groq_api_key:
